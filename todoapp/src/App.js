c1e7766b4cc80a217511818ddfa9b85cf6b3508d
dev <dev@test.com>
2022-07-03 22:19:08 +0300

    ToDoApp

---
+++ todoapp/src/App.js
@@ -0,0 +1,116 @@
+import "./App.css";
+import React, { Component } from "react";
+
+export default class App extends Component {
+  state = {
+    task: "",
+    data: [],
+    show: "none",
+  };
+
+  Add = () => {
+    if (this.state.task != "") {
+      var today = new Date();
+      var date =
+        today.getFullYear() +
+        "/" +
+        (today.getMonth() + 1) +
+        "/" +
+        today.getDate();
+
+      var time =
+        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+      let task = {
+        task: this.state.task,
+        date: date,
+        time: time,
+      };
+
+      this.setState({
+        data: [...this.state.data, task],
+        task: "",
+        show: "none",
+      });
+    } else {
+      this.setState({
+        show: "",
+      });
+    }
+  };
+
+  delete = (index) => {
+    var value = index;
+    var arr = this.state.data;
+
+    arr = arr.filter((item, index) => index !== value);
+
+    this.setState({
+      data: arr,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h1>ToDo List</h1>
+                <div
+                  class="alert alert-danger"
+                  role="alert"
+                  style={{ display: this.state.show }}
+                >
+                  Please enter your task !!!!!!
+                </div>
+                <input
+                  placeholder="Enter your task"
+                  className="form-control"
+                  value={this.state.task}
+                  onChange={(e) => {
+                    this.setState({
+                      task: e.target.value,
+                    });
+                  }}
+                ></input>
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  onClick={this.Add}
+                >
+                  Enter
+                </button>
+              </div>
+              <div className="col">
+                <ul className="list-group">
+                  {this.state.data.map((data, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="list-group-item list-group-item-primary"
+                      >
+                        {data.task}
+                        <span>
+                          {data.date} {data.time}
+                        </span>
+                        <button
+                          type="button"
+                          className="btn btn-danger"
+                          onClick={() => this.delete(index)}
+                        >
+                          Delete
+                        </button>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
